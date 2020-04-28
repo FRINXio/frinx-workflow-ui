51af14ef9b4d238b4cc3241e855e4faeee4987d6
Pavel Paroulek <pparoulek@frinx.io>
2020-04-28 16:32:44 +0200

    Adding additional type

---
+++ src/common/flowtypes.js
@@ -0,0 +1,13 @@
+// @flow
+
+export type Task = {
+  taskType: string,
+  status: string,
+  reasonForIncompletion: string,
+  referenceTaskName: string,
+  callbackAfterSeconds: number,
+  pollCount: number,
+  logs: {},
+  inputData: {},
+  outputData: {}
+};
