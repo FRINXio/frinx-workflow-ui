37bd24782355931c6bd8f8ab711ced0f7265cac2
Maros Marsalek <mmarsalek@frinx.io>
2020-05-04 16:56:08 +0200

    Reorder imports to pull css first
    Signed-off-by: Maros Marsalek <mmarsalek@frinx.io>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 // @flow
+import './css/bootstrap.min.css';
+import './css/awesomefonts.css';
+import './css/neat.css';
+import './css/mono-blue.min.css';
 import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
@@ -11,10 +15,6 @@ import bulkReducer from "./store/reducers/bulk";
 import mountedDeviceReducer from "./store/reducers/mountedDevices";
 import searchReducer from "./store/reducers/searchExecs";
 import { frontendUrlPrefix } from "./constants";
-import './css/bootstrap.min.css';
-import './css/mono-blue.min.css';
-import './css/neat.css';
-import './css/awesomefonts.css';
 
 const rootReducer = combineReducers({
   bulkReducer,
