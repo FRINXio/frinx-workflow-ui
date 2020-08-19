6d0bea8c75a23f10bf0463d18cc2fb63466607d6
Adam Chmara <adam.chmara1@gmail.com>
2020-08-19 16:16:20 +0200

    Add UniFlow label next to logo in header
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>

--- app/src/common/header/Header.js
+++ app/src/common/header/Header.js
@@ -3,45 +3,58 @@ import { Badge, Navbar } from "react-bootstrap";
 import { withRouter } from "react-router-dom";
 import "./Header.css";
 import logo from "./logo-min.png";
+import x from "./X_icon_RGB-min.png";
 import { version } from "../../../package.json";
+import { motion } from "framer-motion";
+
+const XLetter = () => (
+  <motion.img
+    key={x}
+    src={x}
+    initial={{ opacity: 0.5, x: -27, scale: 0.8 }}
+    animate={{
+      x: 160,
+      scale: 1.1,
+      rotate: 180,
+      opacity: 0,
+    }}
+    transition={{ duration: 0.5, delay: 1.1 }}
+  />
+);
+
+const Logo = () => <motion.img key={logo} src={logo} />;
+
+const Title = () => (
+  <motion.div
+    initial={{ opacity: 0, x: -50 }}
+    animate={{
+      opacity: 1,
+      x: -20,
+    }}
+    transition={{ duration: 0.5, delay: 1.2 }}
+  >
+    <h2>UniFlow</h2>
+  </motion.div>
+);
 
 const Header = () => {
-  const getGreeting = () => {
-    let d = new Date();
-    let time = d.getHours();
-
-    if (time <= 12 && time > 5) {
-      return "Good morning";
-    }
-    if (time > 12 && time <= 17) {
-      return "Good afternoon";
-    }
-    if (time > 17 || time <= 5) {
-      return "Good evening";
-    }
-  };
-
   // workaround to get to parent app context
   const reloadPage = () => {
     history.pushState(null, null, "/");
-    window.location.reload()
-  }
+    window.location.reload();
+  };
 
   return (
     <Navbar className="navbarHeader">
-      <Navbar.Brand>
-        <img alt="logo" src={logo} onClick={() => reloadPage()} style={{ cursor: "pointer" }} />
-          <Badge
-            style={{ fontSize: "55%", marginLeft: "10px" }}
-            variant="light"
-          >
-            {version}
-          </Badge>
+      <Navbar.Brand style={{cursor: "pointer"}} onClick={() => reloadPage()}>
+        <Logo />
+        <XLetter />
       </Navbar.Brand>
+      <Title />
       <Navbar.Toggle />
       <Navbar.Collapse className="justify-content-end">
         <Navbar.Text style={{ textAlign: "right" }}>
-          {getGreeting()}
+          <Badge variant="light">{version}</Badge>
           <br />
         </Navbar.Text>
       </Navbar.Collapse>
