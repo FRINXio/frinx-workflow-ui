02bb7de075dd4dacd581bb95633a993bcd016d2c
Adam Chmara <adam.chmara1@gmail.com>
2020-08-20 15:21:57 +0200

    Adjust header titles and animations
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>

--- app/src/common/header/Header.js
+++ app/src/common/header/Header.js
@@ -3,35 +3,19 @@ import { Badge, Navbar } from "react-bootstrap";
 import { withRouter } from "react-router-dom";
 import "./Header.css";
 import logo from "./logo-min.png";
-import x from "./X_icon_RGB-min.png";
 import { version } from "../../../package.json";
 import { motion } from "framer-motion";
-
-const XLetter = () => (
-  <motion.img
-    key={x}
-    src={x}
-    initial={{ opacity: 0.5, x: -27, scale: 0.8 }}
-    animate={{
-      x: 160,
-      scale: 1.1,
-      rotate: 180,
-      opacity: 0,
-    }}
-    transition={{ duration: 0.5, delay: 1.1 }}
-  />
-);
 
 const Logo = () => <motion.img key={logo} src={logo} />;
 
 const Title = () => (
   <motion.div
-    initial={{ opacity: 0, x: -50 }}
+    initial={{ opacity: 0, x: -30 }}
     animate={{
       opacity: 1,
-      x: -20,
+      x: 0,
     }}
-    transition={{ duration: 0.5, delay: 1.2 }}
+    transition={{ duration: 0.5, delay: 0.2 }}
   >
     <h2>UniFlow</h2>
   </motion.div>
@@ -48,7 +32,6 @@ const Header = () => {
     <Navbar className="navbarHeader">
       <Navbar.Brand style={{cursor: "pointer"}} onClick={() => reloadPage()}>
         <Logo />
-        <XLetter />
       </Navbar.Brand>
       <Title />
       <Navbar.Toggle />
