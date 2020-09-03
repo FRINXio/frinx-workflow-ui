6040b43dd0e9d30b5c8d575de1a24e659b470505
Tomas Olvecky <tomas.olvecky@gmail.com>
2020-09-03 12:04:55 +0200

    Linting

--- app/src/pages/workflowList/Scheduling/Scheduling.js
+++ app/src/pages/workflowList/Scheduling/Scheduling.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useContext } from "react";
+// @flow
+
+import PageCount from '../../../common/PageCount';
+import PageSelect from '../../../common/PageSelect';
+import React, {useContext, useEffect, useState} from 'react';
+import SchedulingModal from './SchedulingModal/SchedulingModal';
+import superagent from 'superagent';
 import {
   Accordion,
   Button,
@@ -6,32 +12,30 @@ import {
   Col,
   Container,
   Row,
-  Table
-} from "react-bootstrap";
-import { withRouter } from "react-router-dom";
-import PageCount from "../../../common/PageCount";
-import PageSelect from "../../../common/PageSelect";
-import SchedulingModal from "./SchedulingModal/SchedulingModal";
-import superagent from "superagent";
-import { GlobalContext } from '../../../common/GlobalContext'
+  Table,
+} from 'react-bootstrap';
+import {GlobalContext} from '../../../common/GlobalContext';
+import {withRouter} from 'react-router-dom';
 
-const Scheduling = props => {
-  const global = useContext(GlobalContext)
+const Scheduling = () => {
+  const global = useContext(GlobalContext);
   const [showSchedulingModal, setShowSchedulingModal] = useState(false);
   const [activeRow, setActiveRow] = useState();
   const [pagesCount, setPagesCount] = useState(1);
   const [data, setData] = useState(undefined);
+  // TODO: display error
   const [error, setError] = useState(undefined);
   const [defaultPages, setDefaultPages] = useState(20);
   const [viewedPage, setViewedPage] = useState(1);
 
   const refresh = () => {
-    const path = global.backendApiUrlPrefix + "/schedule/";
-    const req = superagent.get(path).accept("application/json");
+    const path = global.backendApiUrlPrefix + '/schedule/';
+    const req = superagent.get(path).accept('application/json');
     req.end((err, res) => {
       if (res && res.ok && Array.isArray(res.body)) {
         const result = res.body;
-        let dataset = result.sort((a, b) =>
+        const dataset =
+          result.sort((a, b) =>
             a.name > b.name ? 1 : b.name > a.name ? -1 : 0
           ) || [];
         let size = Math.floor(dataset.length / defaultPages);
@@ -40,24 +44,25 @@ const Scheduling = props => {
         deselectActiveRow();
       } else {
         console.log('err res', res, 'err', err);
-        const newError = (err != null) ? 'Network error: ' + err : 'Wrong response: ' + res;
+        const newError =
+          err != null ? 'Network error: ' + err : 'Wrong response: ' + res;
         setError(newError);
-        alert(newError);// TODO
+        alert(newError); // TODO
       }
     });
-  }
+  };
 
   useEffect(() => {
     // do network request just once
     refresh();
   }, []);
 
-
   const deselectActiveRow = () => {
     setActiveRow(null);
   };
 
-  const changeActiveRow = (i) => {
+  const changeActiveRow = i => {
+    console.log('changeActiveRow', i);
     const deselectingCurrentRow = activeRow === i;
     if (deselectingCurrentRow) {
       deselectActiveRow();
@@ -72,16 +77,19 @@ const Scheduling = props => {
     setViewedPage(1);
   };
 
-  const deleteEntry = (schedulingEntry) => {
-    const path = global.backendApiUrlPrefix + "/schedule/" + schedulingEntry.name;
-    const req = superagent.delete(path).accept("application/json");
+  const deleteEntry = schedulingEntry => {
+    const path =
+      global.backendApiUrlPrefix + '/schedule/' + schedulingEntry.name;
+    const req = superagent.delete(path).accept('application/json');
     req.end((err, res) => {
       if (res && res.ok) {
         deselectActiveRow();
         refresh();
       } else {
         // TODO
-        alert("Network error");
+        const newError = 'Network error';
+        setError(newError);
+        alert(newError);
       }
     });
   };
@@ -93,38 +101,38 @@ const Scheduling = props => {
   const onModalClose = () => {
     flipShowSchedulingModal();
     refresh();
-  }
+  };
 
   const getActiveScheduleName = () => {
     if (activeRow != null && data[activeRow] != null) {
-      return data[activeRow]["name"];
+      return data[activeRow]['name'];
     }
     return null;
-  }
+  };
 
   const getActiveWorkflowName = () => {
     if (activeRow != null && data[activeRow] != null) {
-      return data[activeRow]["workflowName"];
+      return data[activeRow]['workflowName'];
     }
     return null;
-  }
+  };
 
   const getActiveWorkflowVersion = () => {
     if (activeRow != null && data[activeRow] != null) {
-      return data[activeRow]["workflowVersion"];
+      return data[activeRow]['workflowVersion'];
     }
     return null;
-  }
+  };
 
   const getDataLength = () => {
     if (data != null) {
       return data.length;
     }
     return null;
-  }
+  };
 
   const repeat = () => {
-    let output = [];
+    const output = [];
     if (data != null) {
       for (let i = 0; i < data.length; i++) {
         if (
@@ -139,42 +147,36 @@ const Scheduling = props => {
                 className="clickable wfDef"
                 as={Card.Header}
                 variant="link"
-                eventKey={i}
-              >
-                <b>{data[i]["workflowName"]}</b> v.{data[i]["workflowVersion"]}
+                eventKey={i}>
+                <b>{data[i]['workflowName']}</b> v.{data[i]['workflowVersion']}
                 <br />
-                <div className="description">
-                  { data[i]["cronString"] }
-                </div>
+                <div className="description">{data[i]['cronString']}</div>
               </Accordion.Toggle>
               <Accordion.Collapse eventKey={i}>
-                <Card.Body style={{ padding: "0px" }}>
+                <Card.Body style={{padding: '0px'}}>
                   <div
                     style={{
                       background:
-                        "linear-gradient(-120deg, rgb(0, 147, 255) 0%, rgb(0, 118, 203) 100%)",
-                      padding: "15px",
-                      marginBottom: "10px"
-                    }}
-                  >
+                        'linear-gradient(-120deg, rgb(0, 147, 255) 0%, rgb(0, 118, 203) 100%)',
+                      padding: '15px',
+                      marginBottom: '10px',
+                    }}>
                     <Button
                       variant="outline-light noshadow"
-                      onClick={flipShowSchedulingModal}
-                    >
+                      onClick={flipShowSchedulingModal}>
                       Edit
                     </Button>
 
                     <Button
                       variant="outline-danger noshadow"
-                      style={{ float: "right" }}
-                      onClick={deleteEntry.bind(this, data[i])}
-                    >
+                      style={{float: 'right'}}
+                      onClick={deleteEntry.bind(this, data[i])}>
                       <i className="fas fa-trash-alt" />
                     </Button>
                   </div>
                 </Card.Body>
               </Accordion.Collapse>
-            </div>
+            </div>,
           );
         }
       }
@@ -191,12 +193,15 @@ const Scheduling = props => {
         onClose={onModalClose}
         show={showSchedulingModal}
       />
-      <Button variant="outline-primary" style={{ marginLeft: "30px" }}
-      onClick={() => refresh()}>
-        <i className="fas fa-sync" />&nbsp;&nbsp;Refresh
+      <Button
+        variant="outline-primary"
+        style={{marginLeft: '30px'}}
+        onClick={() => refresh()}>
+        <i className="fas fa-sync" />
+        &nbsp;&nbsp;Refresh
       </Button>
 
-      <div className="scrollWrapper" style={{ maxHeight: "650px" }}>
+      <div className="scrollWrapper" style={{maxHeight: '650px'}}>
         <Table>
           <thead>
             <tr>
@@ -204,19 +209,19 @@ const Scheduling = props => {
             </tr>
           </thead>
           <tbody>
-          <tr><td style={{padding: "0"}}><Accordion activeKey={activeRow}>
-            {repeat()}
-          </Accordion></td></tr>
+            <tr>
+              <td style={{padding: '0'}}>
+                <Accordion activeKey={activeRow}>{repeat()}</Accordion>
+              </td>
+            </tr>
           </tbody>
         </Table>
       </div>
-      <Container style={{ marginTop: "5px" }}>
+      <Container style={{marginTop: '5px'}}>
         <Row>
           <Col sm={2}>
             <PageCount
-              dataSize={
-                getDataLength()
-              }
+              dataSize={getDataLength()}
               defaultPages={defaultPages}
               handler={setCountPages.bind(this)}
             />
@@ -233,6 +238,6 @@ const Scheduling = props => {
       </Container>
     </div>
   );
-}
+};
 
 export default withRouter(Scheduling);
--- app/src/pages/workflowList/Scheduling/SchedulingModal/SchedulingModal.js
+++ app/src/pages/workflowList/Scheduling/SchedulingModal/SchedulingModal.js
@@ -1,10 +1,12 @@
-import React, { useState, useContext } from "react";
-import { Button, Modal, Form } from "react-bootstrap";
-import superagent from "superagent";
-import { GlobalContext } from '../../../../common/GlobalContext';
+// @flow
 
-const stateSubmit = "Submit";
-const stateSubmitting = "Submitting..."
+import React, {useContext, useState} from 'react';
+import superagent from 'superagent';
+import {Button, Form, Modal} from 'react-bootstrap';
+import {GlobalContext} from '../../../../common/GlobalContext';
+
+const stateSubmit = 'Submit';
+const stateSubmitting = 'Submitting...';
 
 const SchedulingModal = props => {
   const global = useContext(GlobalContext);
@@ -12,7 +14,7 @@ const SchedulingModal = props => {
   const [status, setStatus] = useState();
   const [error, setError] = useState();
 
-  const DEFAULT_CRON_STRING = '* * * * *'
+  const DEFAULT_CRON_STRING = '* * * * *';
 
   const handleClose = () => {
     props.onClose();
@@ -22,8 +24,8 @@ const SchedulingModal = props => {
     setSchedule(null);
     setStatus(stateSubmit);
     setError(null);
-    const path = global.backendApiUrlPrefix + "/schedule/" + props.name;
-    const req = superagent.get(path).accept("application/json");
+    const path = global.backendApiUrlPrefix + '/schedule/' + props.name;
+    const req = superagent.get(path).accept('application/json');
     req.end((err, res) => {
       if (res && res.ok) {
         // found in db
@@ -38,42 +40,44 @@ const SchedulingModal = props => {
           // new schedule is created with enabled: true due to
           // https://github.com/flaviostutz/schellar/issues/5
           enabled: true,
-          cronString: DEFAULT_CRON_STRING
-        })
+          cronString: DEFAULT_CRON_STRING,
+        });
       }
     });
-  }
+  };
 
   const submitForm = () => {
     setError(null);
     setStatus(stateSubmitting);
-    const path = global.backendApiUrlPrefix + "/schedule/" + props.name;
-    const req = superagent.put(path, schedule).set("Content-Type", "application/json");
+    const path = global.backendApiUrlPrefix + '/schedule/' + props.name;
+    const req = superagent
+      .put(path, schedule)
+      .set('Content-Type', 'application/json');
     req.end((err, res) => {
       if (res && res.ok) {
         handleClose();
       } else {
         setStatus(stateSubmit);
-        setError("Request failed:" + err);
+        setError('Request failed:' + err);
       }
     });
-  }
+  };
 
-  const setCronString = (str) => {
-    let mySchedule = {
+  const setCronString = str => {
+    const mySchedule = {
       ...schedule,
-      cronString: str
+      cronString: str,
     };
     setSchedule(mySchedule);
-  }
+  };
 
-  const setEnabled = (enabled) => {
-    let mySchedule = {
+  const setEnabled = enabled => {
+    const mySchedule = {
       ...schedule,
-      enabled: enabled
+      enabled: enabled,
     };
     setSchedule(mySchedule);
-  }
+  };
 
   const getCronString = () => {
     if (schedule != null) {
@@ -82,21 +86,25 @@ const SchedulingModal = props => {
       }
     }
     return DEFAULT_CRON_STRING;
-  }
+  };
 
   const getCrontabGuruUrl = () => {
     const url = 'https://crontab.guru/#' + getCronString().replace(/\s/g, '_');
-    return <a target="_blank" href={url}>crontab.guru</a>;
-  }
+    return (
+      <a target="_blank" href={url}>
+        crontab.guru
+      </a>
+    );
+  };
 
   const getEnabled = () => {
     if (schedule != null) {
-      if (typeof schedule.enabled === "boolean") {
+      if (typeof schedule.enabled === 'boolean') {
         return schedule.enabled;
       } // backend does not send this property when disabled
     }
     return false;
-  }
+  };
 
   return (
     <Modal
@@ -104,8 +112,7 @@ const SchedulingModal = props => {
       dialogClassName="modal-70w"
       show={props.show}
       onHide={handleClose}
-      onShow={handleShow}
-    >
+      onShow={handleShow}>
       <Modal.Header>
         <Modal.Title>Schedule Details - {props.name}</Modal.Title>
       </Modal.Header>
@@ -132,24 +139,20 @@ const SchedulingModal = props => {
         </Form>
       </Modal.Body>
       <Modal.Footer>
-        <pre>
-        {error}
-        </pre>
-
+        <pre>{error}</pre>
         <Button
-          variant={status === stateSubmit ? "primary" : "info" }
+          variant={status === stateSubmit ? 'primary' : 'info'}
           onClick={submitForm}
-          disabled={status === stateSubmitting}
-        >
+          disabled={status === stateSubmitting}>
           {status === stateSubmit ? <i className="fas fa-play" /> : null}
-          {status === stateSubmitting ?
-          (<i className="fas fa-spinner fa-spin" />) : null}
+          {status === stateSubmitting ? (
+            <i className="fas fa-spinner fa-spin" />
+          ) : null}
           &nbsp;&nbsp;{status}
         </Button>
         <Button variant="secondary" onClick={handleClose}>
           Close
         </Button>
-
       </Modal.Footer>
     </Modal>
   );
