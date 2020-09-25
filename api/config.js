811be10fc659112d6d6f4a92f84d898fedb6cc5c
Adam Chmara <adam.chmara1@gmail.com>
2020-09-25 14:48:39 +0200

    Separation of API and server
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>

--- api/config.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
-const env = process.env;
-const conf = {
-  conductorHost: env.CONDUCTOR_HOST || "conductor-server:8080/api/",
-};
-
-module.exports = conf;
