00b3691c1538cc9d9e23863da8a473e611e0bf7e
Adam Chmara <adam.chmara1@gmail.com>
2020-10-08 15:50:32 +0200

    Advanced task parameters hidden by default
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>

--- src/pages/diagramBuilder/NodeModal/GeneralTab.js
+++ src/pages/diagramBuilder/NodeModal/GeneralTab.js
@@ -1,9 +1,10 @@
 // @flow
-import React from 'react';
+import React, {useState} from 'react';
+import {Accordion, Icon} from 'semantic-ui-react';
 import {Button, ButtonGroup, Col, Form, InputGroup} from 'react-bootstrap';
 import {taskDescriptions} from '../../../constants';
 
-const NOT_GENERAL = [
+const HIDDEN = [
   'type',
   'subWorkflowParam',
   'joinOn',
@@ -15,6 +16,7 @@ const NOT_GENERAL = [
 ];
 
 const GeneralTab = props => {
+  const [showAdvancedParams, setShowAdvancedParams] = useState(false);
   const taskName = props.inputs?.name || '';
   const taskRefName = props?.inputs?.taskReferenceName || '';
   const decisionCases = [];
@@ -81,101 +83,125 @@ const GeneralTab = props => {
     </Form.Group>
   );
 
+  const renderDecisionCasesParam = item => {
+    return Object.entries(item[1]).forEach(entry => {
+      decisionCases.push(
+        <Col sm={6} key={`colGeneral-decision-case`}>
+          <Form.Group>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text>is equal to</InputGroup.Text>
+              </InputGroup.Prepend>
+              <Form.Control
+                type="input"
+                onChange={e => props.handleInput(e.target.value, item)}
+                value={entry[0]}
+              />
+            </InputGroup>
+            <Form.Text className="text-muted">
+              {taskDescriptions[item[0]]}
+            </Form.Text>
+          </Form.Group>
+        </Col>,
+      );
+    });
+  };
+
+  const renderBooleanParam = (item, i) => {
+    return (
+      <Col sm={6} key={`colGeneral-boolean-${i}`}>
+        {buttonWrappedField(item, ['<', !item[1]], ['>', !item[1]])}
+      </Col>
+    );
+  };
+
+  const renderCaseValueParam = item => {
+    caseValueParam.push(
+      <Col sm={6} key={`colGeneral-case-value`}>
+        <Form.Group>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>if</InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control
+              type="input"
+              onChange={e => props.handleInput(e.target.value, item[0])}
+              value={item[1]}
+            />
+          </InputGroup>
+          <Form.Text className="text-muted">
+            {taskDescriptions[item[0]]}
+          </Form.Text>
+        </Form.Group>
+      </Col>,
+    );
+  };
+
+  const renderParam = (item, i) => {
+    return (
+      <Col sm={6} key={`colGeneral-param-${i}`}>
+        <Form.Group>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>{item[0]}:</InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control
+              type="input"
+              onChange={e => props.handleInput(e.target.value, item[0])}
+              value={item[1]}
+            />
+          </InputGroup>
+          <Form.Text className="text-muted">
+            {taskDescriptions[item[0]]}
+          </Form.Text>
+        </Form.Group>
+      </Col>
+    );
+  };
+
+  const handleGeneralParams = (item, i) => {
+    const BOOLEAN_PARAMS = ['optional', 'asyncComplete'];
+
+    if (BOOLEAN_PARAMS.includes(item[0])) {
+      return renderBooleanParam(item, i);
+    }
+
+    switch (item[0]) {
+      case 'caseValueParam':
+        return renderCaseValueParam(item);
+      case 'decisionCases':
+        return renderDecisionCasesParam(item);
+      default:
+        return renderParam(item, i);
+    }
+  };
+
   return (
     <Form>
       {renderTaskName(taskName)}
       {renderTaskRefName(taskRefName)}
-
       <Form.Row>
         {caseValueParam}
         {decisionCases}
       </Form.Row>
-      <hr />
-
-      <Form.Row>
-        {Object.entries(props.inputs).map((item, i) => {
-          if (!NOT_GENERAL.includes(item[0])) {
-            if (item[0] === 'decisionCases') {
-              return Object.entries(item[1]).forEach((entry, i) => {
-                decisionCases.push(
-                  <Col sm={6} key={`colGeneral-${i}`}>
-                    <Form.Group>
-                      <InputGroup>
-                        <InputGroup.Prepend>
-                          <InputGroup.Text>is equal to</InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <Form.Control
-                          type="input"
-                          onChange={e =>
-                            props.handleInput(e.target.value, item)
-                          }
-                          value={entry[0]}
-                        />
-                      </InputGroup>
-                      <Form.Text className="text-muted">
-                        {taskDescriptions[item[0]]}
-                      </Form.Text>
-                    </Form.Group>
-                  </Col>,
-                );
-              });
-            } else {
-              if (item[0] === 'optional') {
-                return (
-                  <Col sm={6} key={`colGeneral-${i}`}>
-                    {buttonWrappedField(item, ['<', !item[1]], ['>', !item[1]])}
-                  </Col>
-                );
-              } else if (item[0] === 'caseValueParam') {
-                caseValueParam.push(
-                  <Col sm={6} key={`colGeneral-${i}`}>
-                    <Form.Group>
-                      <InputGroup>
-                        <InputGroup.Prepend>
-                          <InputGroup.Text>if</InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <Form.Control
-                          type="input"
-                          onChange={e =>
-                            props.handleInput(e.target.value, item[0])
-                          }
-                          value={item[1]}
-                        />
-                      </InputGroup>
-                      <Form.Text className="text-muted">
-                        {taskDescriptions[item[0]]}
-                      </Form.Text>
-                    </Form.Group>
-                  </Col>,
-                );
-              } else {
-                return (
-                  <Col sm={6} key={`colGeneral-${i}`}>
-                    <Form.Group>
-                      <InputGroup>
-                        <InputGroup.Prepend>
-                          <InputGroup.Text>{item[0]}:</InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <Form.Control
-                          type="input"
-                          onChange={e =>
-                            props.handleInput(e.target.value, item[0])
-                          }
-                          value={item[1]}
-                        />
-                      </InputGroup>
-                      <Form.Text className="text-muted">
-                        {taskDescriptions[item[0]]}
-                      </Form.Text>
-                    </Form.Group>
-                  </Col>
-                );
+      <Accordion>
+        <Accordion.Title
+          active={showAdvancedParams}
+          onClick={() => setShowAdvancedParams(!showAdvancedParams)}>
+          <Icon name="dropdown" />
+          Advanced parameters
+        </Accordion.Title>
+        <Accordion.Content active={showAdvancedParams}>
+          <Form.Row>
+            {Object.entries(props.inputs).map((item, i) => {
+              if (!HIDDEN.includes(item[0])) {
+                return handleGeneralParams(item, i);
               }
-            }
-          }
-          return null;
-        })}
-      </Form.Row>
+              return null;
+            })}
+          </Form.Row>
+        </Accordion.Content>
+      </Accordion>
     </Form>
   );
 };
